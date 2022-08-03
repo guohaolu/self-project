9e3056f3b29fa8d2500811a9d2bc5910be2b882e
PC <1670212878@qq.com>
2022-08-03 12:27:02 +0800

    clean code

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -6,13 +6,9 @@ public class BinarySearch {
         int mid;
         while (left <= right) {
             mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            if (arr[mid] == target) { return mid; }
+            else if (arr[mid] < target) { left = mid + 1; }
+            else { right = mid - 1; }
         }
         return -1;
     }
