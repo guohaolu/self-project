f8cb1734e7475cdd1ad3148cddaf7d24358fd89e
guohaolu <1670212878@qq.com>
2024-02-08 15:34:43 +0800

    commit

--- algorithm-design/src/main/java/org/example/tree/DequeDemo.java
+++ algorithm-design/src/main/java/org/example/tree/DequeDemo.java
@@ -5,6 +5,9 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 双端队列输出的顺序：按照从首到尾
+ */
 public class DequeDemo {
     public static void main(String[] args) {
         List<Integer> ls = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
